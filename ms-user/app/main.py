b34678883e4242b35da50ff6a303e38f63a4ed81
jemaie <jerome.maier@uzh.ch>
2024-03-11 19:57:54 +0100

    initial database setup for authentication and user

--- ms-user/app/main.py
+++ ms-user/app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic import BaseModel
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 import os
+
+
+client = AsyncIOMotorClient(os.getenv("MONGO_URL", "mongodb://localhost:27017"))
+db = client.ms_user_db
+collection = db.user_collection
+
 
 # FastAPI app
 app = FastAPI()
