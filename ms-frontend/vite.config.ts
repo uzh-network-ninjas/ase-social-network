f716466b3ac650ad0eb2da3fab15bb7f457d1c8a
kristijonas <kbuciuna@ethz.ch>
2024-03-13 12:29:05 +0100

    Commiting everything to my own branch

--- ms-frontend/vite.config.ts
+++ ms-frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/viteplugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
