89047b36e3b05d29f0f928f3b327eac525dea14c
bauerDOTuzh <91192620+bauerDOTuzh@users.noreply.github.com>
2024-03-20 13:37:51 +0100

    fix - parsing from url

--- ms-user/app/UserController.py
+++ ms-user/app/UserController.py
@@ -52,7 +52,7 @@ async def upload_user_image(user_id: str = Path(...), file: UploadFile = File(..
     # get data from env (doesn't matter if localstack or true aws )
     s3_client = boto3.client(
         's3',
-        endpoint_url="http://localstack:4566",#os.getenv("S3_ENDPOINT_URL"),
+        endpoint_url= os.getenv("S3_ENDPOINT_URL"),
         aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID", "test"),
         aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY", "test"),
         region_name=os.getenv("AWS_DEFAULT_REGION", "us-east-1")
