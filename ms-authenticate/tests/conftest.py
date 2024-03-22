6fad2ceff246f9de9b9a416e484980871d47bef7
bauerDOTuzh <91192620+bauerDOTuzh@users.noreply.github.com>
2024-03-22 13:27:32 +0100

    cleaning

--- ms-authenticate/tests/conftest.py
+++ ms-authenticate/tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from fastapi.testclient import TestClient
 from app.AuthenticateController import app  
 
-# TODO there is still some warning, if want to avoid use explicit verion of httpx (under February 2024)
+#if want to avoid use explicit verion of httpx (until February 2024)
 @pytest.fixture(scope="module")
 def test_app():
     client = TestClient(app)
