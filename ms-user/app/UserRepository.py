692004e01e324cda24afe2f65754b5944023e7fd
jemaie <jerome.maier@uzh.ch>
2024-03-26 13:35:02 +0100

    extract token extraction method and add type to return value of method

--- ms-user/app/UserRepository.py
+++ ms-user/app/UserRepository.py
@@ -1,8 +1,9 @@
 import os
 
+from app.models.User import UserUpdate
 from bson import ObjectId
 from motor.motor_asyncio import AsyncIOMotorClient
-from app.models.User import UserOut, UserUpdate
+from pymongo.results import UpdateResult
 
 class UserRepository:
     def __init__(self):
@@ -10,15 +11,15 @@ class UserRepository:
         db = client.ms_user_db
         self.collection = db.user_collection
 
-    async def find_by_id(self, user_id: str):
+    async def get_user_by_id(self, user_id: str) -> dict:
         return await self.collection.find_one({"_id": ObjectId(user_id)})
 
-    async def find_by_username(self, username: str):
+    async def get_user_by_username(self, username: str) -> dict:
         return await self.collection.find_one({"username": username})
 
-    async def update_user(self, user_id: str, updated_user: UserUpdate):
+    async def update_user_by_id(self, user_id: str, updated_user: UserUpdate) -> UpdateResult:
         updated_userdata = updated_user.model_dump(exclude_unset=True)
         return await self.collection.update_one({"_id": ObjectId(user_id)}, {"$set": updated_userdata})
 
-    async def delete_user_by_id(self, user_id: str):
+    async def delete_user_by_id(self, user_id: str) -> dict:
         return await self.collection.find_one_and_delete({"_id": ObjectId(user_id)})
--- ms-user/app/UserService.py
+++ ms-user/app/UserService.py
@@ -1,33 +1,45 @@
-from fastapi import HTTPException
+import jwt
+
 from app.models.User import UserOut, UserUpdate
 from app.UserRepository import UserRepository
+from fastapi import HTTPException, Request
 
 class UserService:
     def __init__(self):
         self.ur = UserRepository()
 
-    async def get_user_by_id(self, user_id: str):
-        result = await self.ur.find_by_id(user_id)
+    async def get_user_by_id(self, user_id: str) -> UserOut:
+        result = await self.ur.get_user_by_id(user_id)
+        print(result)
         if not result:
             raise HTTPException(status_code=404, detail="User not found!")
         result["id"] = str(result["_id"])
         return UserOut(**result)
 
-    async def get_user_by_username(self, username: str):
-        result = await self.ur.find_by_username(username)
+    async def get_user_by_username(self, username: str) -> UserOut:
+        result = await self.ur.get_user_by_username(username)
         if not result:
             raise HTTPException(status_code=404, detail="User not found!")
         result["id"] = str(result["_id"])
         return UserOut(**result)
 
-    async def update_user_by_id(self, user_id: str, updated_user: UserUpdate):
-        result = await self.ur.update_user(user_id, updated_user)
+    #TODO Adjust method as it probably always returns user, even on failure of insertion
+    async def update_user_by_id(self, user_id: str, updated_user: UserUpdate) -> UserUpdate:
+        result = await self.ur.update_user_by_id(user_id, updated_user)
         if not result:
             raise HTTPException(status_code=404, detail="User not found!")
         return updated_user
 
-    async def delete_user_by_id(self, user_id):
+    async def delete_user_by_id(self, user_id: str) -> dict[str, str]:
         result = await self.ur.delete_user_by_id(user_id)
         if not result:
             raise HTTPException(status_code=404, detail="User not found!")
         return {"deleted": result["username"]}
+
+    @staticmethod
+    def extract_user_id_from_token(request: Request) -> str:
+        bearer = request.headers.get("Authorization")
+        token = bearer.split(" ")[1]
+        payload = jwt.decode(token, options={"verify_signature": False})
+        user_id = payload["sub"]
+        return user_id
