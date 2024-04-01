37b79aa280313713cca826d44abcea4954fe612f
jemaie <jerome.maier@uzh.ch>
2024-04-01 18:55:57 +0200

    closes #98; getting and creating reviews & final setup of settings for it

--- ms-review/app/ReviewController.py
+++ ms-review/app/ReviewController.py
@@ -1,24 +1,24 @@
 from typing import List, Annotated
 
 from app.models.Review import ReviewCreate, ReviewOut
-from app.ReviewService import ReviewService
+from ReviewService import ReviewService
 from fastapi import FastAPI, Request, UploadFile, Form
 
 app = FastAPI()
 rs = ReviewService()
 
-@app.post("/")
-async def create_review(request: Request):
+@app.post("/", response_model=ReviewOut)
+async def create_review(request: Request, review: ReviewCreate) -> ReviewOut:
     user_id = rs.extract_user_id_from_token(request)
-    raise NotImplementedError
+    return await rs.create_review(user_id, review)
 
-@app.patch("/image")
-async def append_review_image(review_id: Annotated[str, Form()], image: UploadFile):
-    raise NotImplementedError
+@app.patch("/image", response_model=ReviewOut)
+async def append_review_image(review_id: Annotated[str, Form()], image: UploadFile) -> ReviewOut:
+    return await rs.append_review_image_by_id(review_id, image)
 
-@app.get("/{review_id}")
-async def get_review(review_id: str):
-    raise NotImplementedError
+@app.get("/{review_id}", response_model=ReviewOut)
+async def get_review(review_id: str) -> ReviewOut:
+    return await rs.get_review_by_id(review_id)
 
 @app.get("/")
 async def get_feed(request: Request, token: str):
--- ms-review/app/ReviewRepository.py
+++ ms-review/app/ReviewRepository.py
@@ -1,9 +1,11 @@
 import os
 
-from app.models.Review import ReviewCreate, ReviewOut
+from app.models.Review import ReviewCreate, ReviewCreateImage, ReviewOut
 from bson import ObjectId
 from datetime import datetime
 from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo.results import UpdateResult, InsertOneResult
+
 
 class ReviewRepository:
     def __init__(self):
@@ -11,11 +13,14 @@ class ReviewRepository:
         db = client.ms_review_db
         self.collection = db.review_collection
 
-    async def add_review(self, review: ReviewCreate) -> ReviewOut:
-        raise NotImplementedError
+    async def add_review(self, user_id: str, review: ReviewCreate) -> InsertOneResult:
+        review_dict = review.model_dump()
+        review_dict["user_id"] = user_id
+        return await self.collection.insert_one(review_dict)
 
-    async def add_review_image(self, review: ReviewCreate) -> ReviewOut:
-        raise NotImplementedError
+    async def update_review_image(self, review_id: str, review_image: ReviewCreateImage) -> UpdateResult:
+        updated_review = review_image.model_dump()
+        return await self.collection.update_one({"_id": ObjectId(review_id)}, {"$set": updated_review})
 
     async def get_review_by_id(self, review_id: str) -> dict:
-        raise await self.collection.find_one({"_id": ObjectId(review_id)})
+        return await self.collection.find_one({"_id": ObjectId(review_id)})
