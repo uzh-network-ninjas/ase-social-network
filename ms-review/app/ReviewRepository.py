3c0b96a0be5fda50040825e9e38a2cbf0b6986e5
jemaie <jerome.maier@uzh.ch>
2024-04-02 22:56:57 +0200

    added pagination logic and created List[ReviewOut] model to return multiple reviews

--- ms-review/app/ReviewRepository.py
+++ ms-review/app/ReviewRepository.py
@@ -1,13 +1,12 @@
 import os
 
-from app.models.ReviewCreate import ReviewCreate
-from app.models.ReviewOut import ReviewOut
-from app.models.ReviewCreateImage import ReviewCreateImage
+from models.ReviewCreate import ReviewCreate
+from models.ReviewCreateImage import ReviewCreateImage
 from bson import ObjectId
 from datetime import datetime
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.results import UpdateResult, InsertOneResult
-
+from typing import List
 
 class ReviewRepository:
     def __init__(self):
@@ -26,3 +25,11 @@ class ReviewRepository:
 
     async def get_review_by_id(self, review_id: str) -> dict:
         return await self.collection.find_one({"_id": ObjectId(review_id)})
+
+    #TODO not sure about return type yet, please investigate :D
+    async def get_feed_by_cursor_and_user_ids(self, timestamp_cursor: datetime, user_ids: List[str], pagesize):
+        query = {
+            "created_at": {"$lt": timestamp_cursor} #,
+            #"user_id": {"$in": user_ids} is this right?
+        }
+        return await self.collection.find(query).sort("created_at", -1).limit(pagesize)
