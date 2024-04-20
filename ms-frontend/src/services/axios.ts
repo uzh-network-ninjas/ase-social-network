9ff5d2e37b611b029b766d135f82be8cf6302c33
bauerDOTuzh <91192620+bauerDOTuzh@users.noreply.github.com>
2024-04-20 16:33:35 +0200

    Revert "test with :8010 for github actions"
    This reverts commit ea9efb12b16de27225322538b963ae340b124ccd.

--- ms-frontend/src/services/axios.ts
+++ ms-frontend/src/services/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useAuthStore } from '@/stores/auth'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8010/'
+  baseURL: 'http://localhost:8000/'
 })
 
 apiClient.interceptors.request.use(
