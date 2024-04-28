cb3697b71dee63754d4fd687b696f99e38c0dfd1
maettuu <mangori41@bluewin.ch>
2024-04-28 20:24:10 +0200

    test ms-user endpoints

--- ms-user/tests/test_controller.py
+++ ms-user/tests/test_controller.py
@@ -4,7 +4,9 @@ from datetime import datetime
 
 # Test data setup
 test_user_id = "test_user_id"
+test_user_id_second = "test_user_id_second"
 test_username = "test_user"
+test_username_second = "test_user_second"
 test_headers = {"Authorization": "Bearer mock_token"}
 test_user = {
     "id": test_user_id,
@@ -18,24 +20,39 @@ test_user = {
     "followers": [],
     "updated_at": None
 }
-
-test_user_update = {
-    "id": test_user_id,
-    "username": test_username,
-    "email": "test@example.com",
+test_user_second = {
+    "id": test_user_id_second,
+    "username": test_username_second,
+    "email": "test_second@example.com",
     "created_at": datetime.now().isoformat(),
     "image": None,
     "preferences": [],
     "restrictions": [],
     "following": [],
     "followers": [],
+    "updated_at": None
+}
+test_follow = {
+    "users": [test_user_second]
+}
+
+test_user_update = {
+    "id": test_user_id,
+    "username": test_username,
+    "email": "test@example.com",
+    "created_at": datetime.now().isoformat(),
+    "image": f"user-images/{test_user_id}/test_image.jpg",
+    "preferences": [],
+    "restrictions": [],
+    "following": [test_user_id_second],
+    "followers": [test_user_id_second],
     "updated_at": datetime.now().isoformat()
 }
 
 
 @pytest.mark.asyncio
 @patch('app.UserService.UserService.get_user_by_id')
-async def test_controller_get_user_success(mock_get_user_by_id, test_app):  # Now using the test_app fixture
+async def test_controller_get_user_success(mock_get_user_by_id, test_app):
     mock_get_user_by_id.return_value = test_user
     response = test_app.get(f"/{test_user_id}")
     assert response.status_code == 200
@@ -44,7 +61,7 @@ async def test_controller_get_user_success(mock_get_user_by_id, test_app):  # No
 
 @pytest.mark.asyncio
 @patch('app.UserService.UserService.get_user_by_username')
-async def test_controller_search_user_success(mock_get_user_by_username, test_app):  # test_app fixture injected
+async def test_controller_search_user_success(mock_get_user_by_username, test_app):
     mock_get_user_by_username.return_value = test_user
     response = test_app.get("/", params={
         "username": test_username
@@ -56,23 +73,77 @@ async def test_controller_search_user_success(mock_get_user_by_username, test_ap
 @pytest.mark.asyncio
 @patch('jwt.decode')  # Mock jwt.decode directly
 @patch('app.UserService.UserService.update_user_by_id')
-async def test_controller_update_user_success(mock_update_user_by_id, mock_jwt_decode, test_app):  # test_app used
+async def test_controller_update_user_success(mock_update_user_by_id, mock_jwt_decode, test_app):
     mock_jwt_decode.return_value = {"sub": test_user_id}  # Mocked user ID to be returned by jwt.decode
-    mock_update_user_by_id.return_value = test_user_update  # Adjust based on actual test update data
-    response = test_app.patch("/", json=test_user, headers=test_headers)  # Make sure this matches the structure of your update payload
+    mock_update_user_by_id.return_value = test_user_update
+    response = test_app.patch("/", json=test_user, headers=test_headers)
     assert response.status_code == 200
     assert response.json() == test_user_update
 
 
 @pytest.mark.asyncio
 @patch('jwt.decode')  # Mock jwt.decode directly
+@patch('app.UserService.UserService.update_user_image_by_id')
+async def test_controller_update_user_image_success(mock_update_user_image_by_id, mock_jwt_decode, test_app):
+    mock_jwt_decode.return_value = {"sub": test_user_id}
+    mock_update_user_image_by_id.return_value = test_user_update
+    with open("/code/tests/assets/test_image.jpg", "rb") as f:
+        response = test_app.patch('/image', files={"image": ("test_image.jpg", f, "image/jpeg")}, headers=test_headers)
+    assert response.status_code == 200
+    assert response.json() == test_user_update
+
+
+@pytest.mark.asyncio
+@patch('jwt.decode')
 @patch('app.UserService.UserService.delete_user_by_id')
 async def test_controller_delete_user_success(mock_delete_user_by_id, mock_jwt_decode, test_app):
-    mock_jwt_decode.return_value = {"sub": test_user_id}  # Mocked user ID to be returned by jwt.decode
-    # Perform the delete request with the Authorization header
+    mock_jwt_decode.return_value = {"sub": test_user_id}
     response = test_app.delete("/", headers=test_headers)
-    # Assertions
     assert response.status_code == 204
     # Verify that the mock methods were called as expected
     mock_delete_user_by_id.assert_called_once()
     mock_jwt_decode.assert_called_once()
+
+
+@pytest.mark.asyncio
+@patch('jwt.decode')
+@patch('app.UserService.UserService.get_following_users_by_id')
+async def test_controller_get_following_users_success(mock_get_following_users_by_id, mock_jwt_decode, test_app):
+    mock_jwt_decode.return_value = {"sub": test_user_id}
+    mock_get_following_users_by_id.return_value = test_follow
+    response = test_app.get(f"/{test_user_id}/following")
+    assert response.status_code == 200
+    assert response.json() == test_follow
+
+
+@pytest.mark.asyncio
+@patch('jwt.decode')
+@patch('app.UserService.UserService.get_user_followers_by_id')
+async def test_controller_get_user_followers_success(mock_get_user_followers_by_id, mock_jwt_decode, test_app):
+    mock_jwt_decode.return_value = {"sub": test_user_id}
+    mock_get_user_followers_by_id.return_value = test_follow
+    response = test_app.get(f"/{test_user_id}/followers")
+    assert response.status_code == 200
+    assert response.json() == test_follow
+
+
+@pytest.mark.asyncio
+@patch('jwt.decode')
+@patch('app.UserService.UserService.follow_user_by_id')
+async def test_controller_follow_user_success(mock_follow_user_by_id, mock_jwt_decode, test_app):
+    mock_jwt_decode.return_value = {"sub": test_user_id}
+    mock_follow_user_by_id.return_value = test_user_update
+    response = test_app.patch(f"/following/{test_user_id}", headers=test_headers)
+    assert response.status_code == 200
+    assert response.json() == test_user_update
+
+
+@pytest.mark.asyncio
+@patch('jwt.decode')
+@patch('app.UserService.UserService.unfollow_user_by_id')
+async def test_controller_unfollow_user_success(mock_unfollow_user_by_id, mock_jwt_decode, test_app):
+    mock_jwt_decode.return_value = {"sub": test_user_id}
+    mock_unfollow_user_by_id.return_value = test_user
+    response = test_app.delete(f"/following/{test_user_id}", headers=test_headers)
+    assert response.status_code == 200
+    assert response.json() == test_user
