a1e8fb47d78feee03b45d8b4ea0a5084b580beed
maettuu <mangori41@bluewin.ch>
2024-05-04 21:16:53 +0200

    call endpoint to update reviews with new username

--- ms-user/app/UserController.py
+++ ms-user/app/UserController.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 from app.models.DietaryCriteria import DietaryCriteria
 from app.models.UserUpdate import UserUpdate
@@ -46,7 +47,7 @@ async def update_user(request: Request, updated_user: UserUpdate) -> UserOut:
     user_id = user_service.extract_user_id_from_token(request)
     user = await user_service.update_user_by_id(user_id, updated_user)
     if updated_user.username:
-        requests.patch(f'http://kong:8000/reviews/update_reviews/{user.username}', headers=request.headers)
+        requests.patch(f'{os.getenv("GATEWAY_IP", "http://kong:8000")}/reviews/update_reviews', headers=request.headers, json={"username": user.username})
     return user
 
 
