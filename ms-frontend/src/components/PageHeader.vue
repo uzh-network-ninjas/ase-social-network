a6ab7690cc1b697f2ddab8a5c77709f27adfd2f9
AlessioBrazerol <alessio.bra@hotmail.ch>
2024-05-06 09:35:21 +0200

    Implemented feed and ensured style consistency.

--- ms-frontend/src/components/PageHeader.vue
+++ ms-frontend/src/components/PageHeader.vue
@@ -12,7 +12,7 @@ defineProps<{
     class="z-40 bg-white py-3 max-sm:border-b max-sm:border-b-medium-emphasis max-sm:px-2 sm:px-4"
   >
     <router-link :to="{ name: 'home' }" custom v-slot="{ navigate }">
-      <Button text rounded :label="label" class="text-2xl font-extralight" @click="navigate">
+      <Button text rounded :label="label" class="text-2xl !font-extralight" @click="navigate">
         <template #icon>
           <BaseIcon icon="chevron-left" />
         </template>
