da2c17ac1b28ecc03ecf3f46dd410ebf648bfde8
bauerDOTuzh <91192620+bauerDOTuzh@users.noreply.github.com>
2024-05-08 13:09:14 +0200

    added e2e for following

--- support/testing/e2etesting/cypress/e2e/follow_users.cy.js
+++ support/testing/e2etesting/cypress/e2e/follow_users.cy.js
@@ -1,63 +1,88 @@
-const endpoint = '/home'
+const endpoint = Cypress.config('baseUrl')
+
+const secondUser = 'user2'
 describe('search a user', () => {
-    beforeEach(() => {
-        cy.loginByUI('user1', 'user');
-      });
+  beforeEach(() => {
+      cy.loginByUI('user1', 'user');
+    });
 
-    it('searches for a user', () => {
-        cy.visit(endpoint)
+  it('searches for a user', () => {
+      cy.visit(endpoint+'/home')
 
-        cy.contains('Home')
-        // todo needs implementation once following fixes
-        // search user by username 'user2 input with placeholder: Search by username
-        // cy.get('input[placeholder="Search by username"]').type('user2')
-        // // click on search button
-        // cy.get('button').contains("Search").click()
+      cy.contains('Home')
+      // get button with "Search User"
+      cy.contains("Search User").click()
+      // wait for uer to be /search-user
+      cy.url().should('include', '/search-user')
+      // search user by username 'user2 input with id "search_user_field"
+      cy.get('input[id="search_user_field"]').type(secondUser) 
 
-        // // assert that button with text 'user2' is visible
-        // cy.get('button').contains("user2").should('be.visible')
+      // press enter
+      cy.get('input[id="search_user_field"]').type('{enter}')
 
-        // // click on button with text 'user2'
-        // cy.get('button').contains("user2").click()
-        // //wait for url to change
-        // cy.url().should('include', '/profile') 
-    })
+      // assert that div with text 'user2' is visible
+      cy.get('div').contains(secondUser).should('be.visible')
 
-
-    // it('can follow a user', () => {
-    //     cy.visit(endpoint)
-    //     // search user by username 'user2 input with placeholder: Search by username
-    //     cy.get('input[placeholder="Search by username"]').type('user2')
-    //     // click on search button
-    //     cy.get('button').contains("Search").click()
-
-
-    //     // click on button with text 'user2'
-    //     cy.get('button').contains("user2").should('be.visible').click()
-    //     cy.url().should('include', '/profile') 
-
-    //     // click on follow button
-    //     cy.get('button').contains("Follow").click()
-    // })
-
-
-    // // register as a seeded user
-    // it('lets one login', () => {
-    //   cy.visit(endpoint)
-    //   cy.contains('Sign In').click()
-    //   cy.url().should('include', '/sign-in')
-  
-    //   cy.get('input[id="profile-name"]').type(userName)
-    //   cy.get('input[id="password"]').type(userPassword)
-  
-    //   cy.get('button').contains("Sign In").click()
-  
-    //   // redirect to / -> sometimes we get redirected to /onboarding, redirect back manually
-  
-    //   cy.url().should('not.eq', endpoint+'/') // kinda a workload ->wait for redirect doesn't matter to what
-  
-    //   cy.visit(endpoint)
-    //   cy.contains('Hello, '+userName)
-    //   // now we open preferences, because the user was seeded
-    // })
+      // click on button with text 'user2'
+      cy.get('div').contains(secondUser).click()
+      //wait for url to change
+      cy.url().should('include', '/profile') 
   })
+
+  it('can follow user', () => {
+    cy.visit(endpoint+'/search-user')
+
+    cy.get('input[id="search_user_field"]').type(secondUser) 
+    cy.get('input[id="search_user_field"]').type('{enter}')
+
+    // assert that div with text 'user2' is visible
+    cy.get('div').contains(secondUser).should('be.visible')
+
+    // click on button with text 'user2'
+    cy.get('div').contains(secondUser).click()
+    //wait for url to change
+    cy.url().should('include', '/profile') 
+
+    cy.contains('div', 'Followers').parent().find('div.text-2xl').then($div => {
+      const initialFollowers = parseInt($div.text());
+      
+      // Click on follow button
+      cy.get('button').contains("Follow").click();
+  
+      // Verify that followers count is initialFollowers + 1
+      cy.contains('div', 'Followers').parent().find('div.text-2xl').should($div => {
+        const newFollowers = parseInt($div.text());
+        expect(newFollowers).to.eq(initialFollowers + 1);
+      });
+    });
+
+  })
+
+  it("after following user2 I follow him as well", () =>{
+    cy.visit(endpoint);
+
+    // Click the button to open the overlay menu
+    // This uses attributes to uniquely identify the button
+    cy.get('button[aria-haspopup="true"][data-pc-name="button"]').click();
+
+    // Ensure the overlay menu is visible before attempting to interact with it
+    cy.get('#overlay_menu').should('be.visible');
+
+    // Click on the 'Profile' link within the overlay menu
+    // Uses role and text to identify the link accurately
+    cy.get('#overlay_menu').find('a').contains('Profile').click();
+
+    // Optionally verify the URL or page content to confirm navigation to the profile
+    cy.url().should('include', '/profile');
+
+    // add delay -> wait for load
+    cy.wait(1000);
+
+    cy.contains('div', 'Follows').parent().find('div.text-2xl').then($div => {
+      const nr_follows = parseInt($div.text());
+      // should be 1
+      expect(nr_follows).to.eq(1);
+    });
+
+  })
+})
